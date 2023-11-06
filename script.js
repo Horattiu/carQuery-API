cd5cad6413b81879bfa9c80a6558ac4b41ab6469
Horattiue <horatiu.oltn@gmail.comm>
2023-11-06 01:50:08 -0800

    err

--- script.js
+++ script.js
@@ -1,102 +1,94 @@
-$(document).ready(function () {
-  const base_url = 'https://www.carqueryapi.com/api/0.3/';
+document.addEventListener("DOMContentLoaded", function () {
+  const base_url = "https://www.carqueryapi.com/api/0.3/";
 
-  function displayModelDetails(make, modelName, year) {
-    $('#model-details').hide();
-    $('.note').hide();
-
-    if (make && modelName) {
-      let urlParams = { cmd: 'getTrims', make: make, model: modelName };
-      if (year) {
-        urlParams.year = year;
-      }
-
-      $.getJSON(base_url + '?callback=?', urlParams, function (data) {
-        if (data.Trims.length === 0) {
-          $('#model-details-content').html(
-            '<p class="error">Please check that you spelled the make and model correctly.</p>'
-          );
-          $('#model-details').show();
-        } else {
-          data.Trims.sort((a, b) => b.model_year - a.model_year);
-
-          let modelDetails = '<table class="tablee">';
-
-          data.Trims.forEach((model) => {
-            modelDetails += `<tr><td class="year" colspan="2">${model.model_year}</td></tr>`;
-
-            for (const key in model) {
-              if (model.hasOwnProperty(key) && model[key]) {
-                modelDetails += `<tr><td class="key">${key.replace(
-                  /_/g,
-                  ' '
-                )}</td><td class="value">${model[key]}</td></tr>`;
-              }
-            }
-          });
-
-          modelDetails += '</table>';
-          $('#model-details-content').html(modelDetails);
-          $('#model-details').show();
-        }
-      });
-    }
+  function showLoading() {
+    document.getElementById("loading-icon").style.display = "block";
+    document.getElementById("search-button").style.display = "none";
   }
 
-  $('#search-button').on('click', function () {
-    const make = $('#car-make').val();
-    const modelName = $('#car-model').val();
-    const year = $('#car-year').val();
-    displayModelDetails(make, modelName, year);
-  });
+  function hideLoading() {
+    document.getElementById("loading-icon").style.display = "none";
+    document.getElementById("search-button").style.display = "block";
+  }
+
+  function displayError(message) {
+    document.getElementById(
+      "model-details-content"
+    ).innerHTML = `<p class="error">${message}</p>`;
+    document.getElementById("model-details").style.display = "block";
+  }
+
+  function displayModelDetails(make, modelName, year) {
+    if (!make || !modelName) {
+      displayError("input field is empty.");
+      return;
+    }
+
+    showLoading();
+
+    let url = new URL(base_url);
+    let params = {
+      cmd: "getTrims",
+      make: make,
+      model: modelName,
+      format: "jsonp", // JSONP format
+      callback: "handleData",
+    };
+    if (year) {
+      params.year = year;
+    }
+    url.search = new URLSearchParams(params).toString();
+
+    // global callback function
+    window.handleData = function (data) {
+      if (data.Trims.length === 0) {
+        displayError(
+          "Please check that you spelled the make and model correctly."
+        );
+      } else {
+        data.Trims.sort(function (a, b) {
+          return b.model_year - a.model_year;
+        });
+
+        let modelDetails = '<table class="tablee">';
+
+        data.Trims.forEach(function (model) {
+          modelDetails += `<tr><td class="year" colspan="2">${model.model_year}</td></tr>`;
+
+          for (const key in model) {
+            if (model.hasOwnProperty(key) && model[key]) {
+              modelDetails += `<tr><td class="key">${key.replace(
+                /_/g,
+                " "
+              )}</td><td class="value">${model[key]}</td></tr>`;
+            }
+          }
+        });
+
+        modelDetails += "</table>";
+        document.getElementById("model-details-content").innerHTML =
+          modelDetails;
+        document.getElementById("model-details").style.display = "block";
+      }
+
+      hideLoading();
+
+      // Remove the callback function from the global scope
+      delete window.handleData;
+    };
+
+    // Make the JSONP request
+    const script = document.createElement("script");
+    script.src = url.toString();
+    document.body.appendChild(script);
+  }
+
+  document
+    .getElementById("search-button")
+    .addEventListener("click", function () {
+      const make = document.getElementById("car-make").value;
+      const modelName = document.getElementById("car-model").value;
+      const year = document.getElementById("car-year").value;
+      displayModelDetails(make, modelName, year);
+    });
 });
-
-// $(document).ready(function () {
-//   const base_url = 'https://www.carqueryapi.com/api/0.3/';
-
-//   function displayModelDetails(make, modelName) {
-//     $('#model-details').hide();
-
-//     if (make && modelName) {
-//       $.getJSON(
-//         base_url + '?callback=?',
-//         { cmd: 'getTrims', make: make, model: modelName },
-//         function (data) {
-//           if (data.Trims.length === 0) {
-//             $('#model-details-content').html(
-//               '<p>Please check that you spelled the make and model correctly.</p>'
-//             );
-//             $('#model-details').show();
-//           } else {
-//             let modelDetails = '<table>';
-
-//             data.Trims.forEach((model) => {
-//               modelDetails += `<tr><td class="year" colspan="2">${model.model_year}</td></tr>`;
-
-//               for (const key in model) {
-//                 if (model.hasOwnProperty(key) && model[key]) {
-//                   modelDetails += `<tr><td class="key">${key.replace(
-//                     /_/g,
-//                     ' '
-//                   )}</td><td class="value">${model[key]}</td></tr>`;
-//                 }
-//               }
-//             });
-
-//             modelDetails += '</table>';
-//             $('#model-details-content').html(modelDetails);
-//             $('#model-details').show();
-//           }
-//         }
-//       );
-//     }
-//   }
-
-//   $('#search-button').on('click', function () {
-//     const make = $('#car-make').val();
-//     const modelName = $('#car-model').val();
-//     displayModelDetails(make, modelName);
-//   });
-// });
-
-// // --------------cea buna mai sus
